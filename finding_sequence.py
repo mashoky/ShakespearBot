9d76884f015240615ff35dbd7a59562756fa9727
Manasa <mashok@caltech.edu>
2016-03-11 10:44:00 -0800

    Fixed hmm simulation to use get random function

--- finding_sequence.py
+++ finding_sequence.py
@@ -2,10 +2,20 @@
 import numpy as np
 from numpy import random
 
+def get_random(row):
+    rand_prob = np.random.uniform(0.0,1.0)
+    print rand_prob
+    cumulative_prob = 0
+    for idx in range(len(row)):
+        cumulative_prob += row[idx]
+        if rand_prob <= cumulative_prob:
+            return idx
+    return row.index(max(row))
+
 def neseq(num_states, num_tokens, pi, A, O, len_seq):
     seq = []
     
-    rand_init_state = np.random.choice(range(num_states))
+    rand_init_state = get_random(pi)
     state = rand_init_state
 
     # states by observations now
@@ -14,8 +24,9 @@ def neseq(num_states, num_tokens, pi, A, O, len_seq):
     # next state essentially chosen randomly, need to find a way 
     # to take probability into account
     while len(seq) < len_seq:
-        rand_obs = np.random.choice(range(num_tokens))
-        seq.append(observation)
+        rand_obs = get_random(Ot[state])
+        seq.append(rand_obs)
+        next_state = get_random(A[state])
         next_state = np.random.choice(range(num_states))
         state = next_state
         
